95db8f14afbad8c71ba564a3712e9c0e6619bc6c
青木直樹 <aokinaoki@aokinaokinoMacBook-puro.local>
2022-05-14 07:23:21 +0900

    1st commit

--- autorun.py
+++ autorun.py
@@ -1,15 +1,29 @@
 import os
-import schedule
+import re
+from this import d
 
+import schedule
 from time import sleep
 from bs4 import BeautifulSoup
 import requests
 
+def line_notify(si, st):
 
-# url = 'https://scraping.official.ec/items/40792454'
-# url = 'https://scraping.official.ec/items/40792284'
+    TOKEN = 'EPIUPC3KnMK3MH0IR2wKIGOSLAokCqbj0dLFnnoI674'
 
+    api_url = 'https://notify-api.line.me/api/notify'
 
+    send_contents = 'Flover:{}, Stock_status:{}'.format(si, st)
+
+    TOKEN_dic = {'Authorization' : 'Bearer' + ' ' + TOKEN}
+    send_dic = {'message': send_contents}
+    print(TOKEN_dic)
+    print(send_dic)
+
+    r = requests.post(api_url, headers=TOKEN_dic, data=send_dic)
+    print(r)
+
+d_dict = []
 def detect_updates():
     nonet_url = 'https://nonet-inc.com/collections/organic-dark-chocolate'
 
@@ -18,9 +32,21 @@ def detect_updates():
     r.raise_for_status()
     soup = BeautifulSoup(r.content, 'lxml')
 
-    # soldout文字列のテキストの取得
+    # 売れきれなのかそうでないのか判断/要素の取得
     new_check_ele = soup.select_one('#product-list-item-7352065425571 > a > div.list-text-wrapper > div.list-meta')
+    # 文字にしておく
+    stock_status = new_check_ele.text
+    # txtファイルに書き込みのに文字列にしておく
     new_check_ele = str(new_check_ele)
+    # シナモン味のタイトル取得
+    sinamon_flover = soup.select_one('#product-list-item-7352065425571 > a > div.list-text-wrapper > div.list-title').text
+    
+    # 改行を消す
+    stock_status = re.sub('\n', ' ', stock_status)
+    sinamon_flover = re.sub('\n', ' ', sinamon_flover)
+    stock_status = stock_status.replace(' ','')
+    sinamon_flover = sinamon_flover.replace(' ', '')
+
     # フォルダのパス
     fold_path = os.path.dirname(os.path.abspath(__file__))
     # textファイルをフォルダのパスごと変数に入れておく
@@ -35,9 +61,10 @@ def detect_updates():
     except:
         comp_check_ele = ''
 
-
+    # 34で取得した文字列とtxtファイル内の文字列を比較する
+    # 同==[変化なし],異==[更新内容をtxtに書き込む]
     if new_check_ele == comp_check_ele:
-        print('変化なし')
+        line_notify(sinamon_flover,stock_status)
         return False
     else:
         with open(comp_path_file, 'w') as f:
@@ -45,11 +72,9 @@ def detect_updates():
             print('更新しました')
             return True
 
-# スケジュールの登録
-schedule.every(1).days.do(detect_updates)
 
+# スケジュールの登録/実行
+schedule.every(5).seconds.do(detect_updates)
 while True:
     schedule.run_pending()
     sleep(1)
-    
-'hjXiOf7HvJkhDmUSy870QFEjJZE62uz7FNbGPwguWmi'
